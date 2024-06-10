8995ec2d0281b9cabea46669fb0c6363f4e67118
ohziiilov3 <klbaskerville0520@gmail.com>
2024-06-09 21:58:51 -0700

    Translate numbers to letters complete

---
+++ script.js
@@ -0,0 +1,34 @@
+const translateNumbers = (encodedMsg) => {
+  let result = "";
+  for (let char of encodedMsg) {
+    if (char === "3") {
+      result += "w";
+    } else if (char === "7") {
+      result += "l";
+    } else {
+        result += char;
+    }
+  }
+  return result;
+};
+
+// Test function 
+// console.log(translateNumbers("Th3 qu1ck br0wn f0x jumps 0v3r th3 l4zy d0g"));
+
+// Add event listener to the button
+
+const decodeBtn = document.querySelector(
+    '#decodeButton'
+  )
+
+  console.dir(decodeBtn);
+
+  decodeBtn.addEventListener('click',() =>{
+//    get encoded msg from input field 
+ const encodedMessage = document.querySelector('#encodedInput').value;
+// Translate the msg 
+const decodedMessage = translateNumbers(encodedMessage);
+console.log(decodedMessage);
+const output = document.querySelector('#decodedOutput');
+output.textContent = decodedMessage;
+  });
