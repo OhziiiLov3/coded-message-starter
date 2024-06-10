f0709c67cc88c486f57579ddcecc64d0de531987
ohziiilov3 <klbaskerville0520@gmail.com>
2024-06-09 22:17:58 -0700

    Step 3: Reversing the words complete

--- script.js
+++ script.js
@@ -12,11 +12,21 @@ const translateNumbers = (encodedMsg) => {
   return result;
 };
 
+
 // Test function 
 // console.log(translateNumbers("Th3 qu1ck br0wn f0x jumps 0v3r th3 l4zy d0g"));
 
-// Add event listener to the button
+const reverseWords = (str) =>{
+  let reverseStr = str.split('').reverse().join("");
+  return reverseStr;
+}
 
+console.log(reverseWords("Zooty"));
+
+
+
+
+// Add event listener to the button
 const decodeBtn = document.querySelector(
     '#decodeButton'
   )
@@ -27,7 +37,7 @@ const decodeBtn = document.querySelector(
 //    get encoded msg from input field 
  const encodedMessage = document.querySelector('#encodedInput').value;
 // Translate the msg 
-const decodedMessage = translateNumbers(encodedMessage);
+const decodedMessage = translateNumbers(reverseWords(encodedMessage));
 console.log(decodedMessage);
 const output = document.querySelector('#decodedOutput');
 output.textContent = decodedMessage;
